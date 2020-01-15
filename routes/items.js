23bad7e6473b4ebbe5fa22b68e13141258d47afd
JuanCamilo08 <jcamilo@intellisys.com.do>
2020-01-15 09:13:59 -0400

    improving the responses in routes

--- routes/items.js
+++ routes/items.js
@@ -13,40 +13,40 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const { error, value } = validateItem(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).json(error.details[0].message);
 
   let item = new itemModel(value);
   item = await item.save();
 
-  res.send(item);
+  res.status(201).send(item);
 });
 
 router.put('/:id', async (req, res) => {
   const { error, value } = validateItem(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).json(error.details[0].message);
 
   let item = await itemModel.findByIdAndUpdate(req.params.id, value, {
     new: true
   });
-  if (!item) return res.status(404).send('The item with the given ID was not found.');
+  if (!item) return res.status(404).json('The item with the given ID was not found.');
 
-  res.send(item);
+  res.status(200).send(item);
 });
 
 router.delete('/:id', async (req, res) => {
   let item = await itemModel.findByIdAndDelete(req.params.id);
 
-  if (!item) return res.status(404).send('The item with the given ID was not found.');
+  if (!item) return res.status(404).json('The item with the given ID was not found.');
 
-  res.send(item);
+  res.status(200).send(item);
 });
 
 router.get('/:id', async (req, res) => {
   const item = await itemModel.findById(req.params.id);
 
-  if (!item) return res.status(404).send('The item with the given ID was not found.');
+  if (!item) return res.status(404).json('The item with the given ID was not found.');
 
-  res.send(item);
+  res.status(200).send(item);
 });
 
 module.exports = router;
--- routes/menus.js
+++ routes/menus.js
@@ -4,35 +4,35 @@ const { menuModel, validateMenu } = require('../models/menu');
 
 router.get('/', async (req, res) => {
   const result = await menuModel.find().select('-__v');
-  res.send(result);
+  res.status(200).send(result);
 });
 
 router.post('/', async (req, res) => {
   const { error, value } = validateMenu(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).json(error.details[0].message);
 
   let result = new menuModel(value);
   result = await result.save();
 
-  res.send(result);
+  res.status(201).send(result);
 });
 
 router.put('/:id', async (req, res) => {
   const { error, value } = validateMenu(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).json(error.details[0].message);
 
   let menu = await menuModel.findByIdAndUpdate(req.params.id, value, { new: true });
-  if (!menu) return res.status(404).send('The menu with the given ID was not found.');
+  if (!menu) return res.status(404).json('The menu with the given ID was not found.');
 
-  res.send(menu);
+  res.status(200).send(menu);
 });
 
 router.delete('/:id', async (req, res) => {
   let menu = await menuModel.findByIdAndDelete(req.params.id);
 
-  if (!menu) return res.status(404).send('The menu with the given ID was not found.');
+  if (!menu) return res.status(404).json('The menu with the given ID was not found.');
 
-  res.send(menu);
+  res.status(200).send(menu);
 });
 
 router.get('/:id', async (req, res) => {
@@ -41,9 +41,9 @@ router.get('/:id', async (req, res) => {
     .select('-__v')
     .populate('items');
 
-  if (!menu) return res.status(404).send('The menu with the given ID was not found.');
+  if (!menu) return res.status(404).json('The menu with the given ID was not found.');
 
-  res.send(menu);
+  res.statuys(200).send(menu);
 });
 
 module.exports = router;
